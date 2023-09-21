2d3bdb8fd783c9de59c5e1dd9172e55843faf15c
posey <posey@DESKTOP-KEAP1HJ>
2023-09-21 11:23:58 +0500

    Добавьте файлы проекта.

---
+++ Задание 1. Метод разделения строки на слова/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите предложение: ");
+        string inputText = Console.ReadLine();
+        string[] words = SplitText(inputText);
+        Console.WriteLine("Слова в предложении: ");
+        PrintWords(words);
+        Console.ReadKey();
+    }
+    //метод разделения строки на слова
+    static string[] SplitText(string text)
+    {
+        //разделения строки по пробелам и удаление пустых элементов
+        string[] words = text.Split(new char[] { ' '}, StringSplitOptions.RemoveEmptyEntries);
+        return words;
+    }
+    //метод для вывода слов на экран
+    static void PrintWords(string[] words)
+    {
+        foreach (string word in words)
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
---
+++ Задание 2. Перестановка слов в предложении/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите предложение: ");
+        string inputPhrase = Console.ReadLine();
+        string reversedPhrase = ReverseWords(inputPhrase);
+        Console.WriteLine("Предложение с переставлеными словами ");
+        Console.WriteLine(reversedPhrase);
+        Console.ReadKey();
+    }
+    //метод разделения предложения на слова
+    static string[] SplitText(string text)
+    {
+        //разделения строки по пробелам и удаления пустых элементов
+        string[] words = text.Split(new char[] {}, StringSplitOptions.RemoveEmptyEntries);
+        return words;
+    }
+    // Метод для перестановки слов в обратной последовательности
+    static string ReverseWords(string inputPhrase)
+    {
+        string[] words = SplitText(inputPhrase);
+        Array.Reverse(words); // Перестановка слов в обратной последовательности
+
+        // Сборка слов обратно в предложение с пробелами
+        string reversedPhrase = string.Join(" ", words);
+
+        return reversedPhrase;
+    }
+}
